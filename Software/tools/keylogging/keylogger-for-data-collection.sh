2bdd1dc7b314e78f257a70a50edb5c13d79b478b
xjjak <xjjak@proton.me>
2024-02-15 19:09:40 +0100

    Keylogging script added
    Collect the filtered output from the QMK driven keyboard using `./hid_listen | grep -E --line-buffered "(0x[A-F0-9]+,)?[0-9]+,[0-9]+,[0-9]{1,2}"` and adds the timestamp given by `date +s%N`.
    
    This script required the program hid_listen to be executable in the same folder and then continuously appends to keylog.csv

---
+++ Software/tools/keylogging/keylogger-for-data-collection.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+get_records(){
+    ./hid_listen | grep -E --line-buffered "(0x[A-F0-9]+,)?[0-9]+,[0-9]+,[0-9]{1,2}" 
+}
+
+prepend_timestamp(){
+    while read line; do
+        echo "$(date +%s%N),$line"
+    done
+}
+
+get_records | prepend_timestamp | tee -a keylog.csv
+
