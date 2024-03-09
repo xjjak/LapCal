eb9283cac5c98d2b99e9b01d1dc01cc46d34272f
Emil Wiards <emil@wiards.net>
2024-03-09 20:38:39 +0100

    Finished keylogging conversion script

--- Software/tools/keylogging/convert-keylog.py
+++ Software/tools/keylogging/convert-keylog.py
@@ -1,10 +1,27 @@
-DATA_OF_RIGHT_HAND = false # set to true to filter for right hand data, instead of left
+# set to true to filter for right hand data, instead of left
+DATA_OF_RIGHT_HAND = False
+
+# List for translation from 
+TRANSLATION = dict(zip(
+    ["00","10","20", "01","11","21", "02","12","22",
+     "03","13","23", "04","14","24", "31","30"],
+    ["00","01","02", "10","11","12", "20","21","22",
+     "30","31","32", "40","41","42", "50","51"]
+))
+
 
 # data entry format: [timestamp, keycode, row, col, layer(?), press_event, ...]
 def preprocess(data):
     new_data = []
     for record in data:
-        new_data.append([record[0], record[1], int(record[2]), int(record[3]), int(record[4]), bool(record[5])])
+        new_data.append([
+            record[0],
+            record[1],
+            int(record[2]),
+            int(record[3]),
+            int(record[4]),
+            bool(record[5])
+        ])
     return new_data
 
 
@@ -19,28 +36,43 @@ def filter_for_hand(data):
             if record[3] < 5:
                 filtered_data.append(record)
 
-def get_data(file_name):
-    data = []
-    with open(file_name, "r") as file:
-        for line in file:
-            data.append(line.strip().split(','))
-    return data
+    return filtered_data
+
+# def get_data(file_name=None):
+#     data = []
+#     if file_name:
+#         with open(file_name, "r") as f:
+#             for line in f.read().strip().split('\n'):
+#                 data.append(line.strip().split(','))
+#     else:
+        
+        
+#     return data
 
 def get_key_number(row, col):
-    number = None
+    global TRANSLATION
+
     if not DATA_OF_RIGHT_HAND:
-        if col <= 2:
-            number = str(col)
-        else:
-            pass # Missing number conversion
-
-
-    return int(number)
-
+        key = str(row) + str(col)
+        number = TRANSLATION[key]
+    else:
+        # TODO: right hand inversion
         
-def generate_output(output_file_name):
-    pass
-    
+    return number
 
 
-data = get_data("keylog.csv")
+if __name__ == "__main__":
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+
+        record = preprocess(line)
+        record = filter_for_hand(record)
+
+        number = get_key_number(record[2], record[3])
+        if record[5]:
+            print(f"+ {number}")
+        else:
+            print(f"- {number}")
