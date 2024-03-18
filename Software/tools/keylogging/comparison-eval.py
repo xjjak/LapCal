555fec3a22041e1f0dda34915756389f6aa1fc67
xjjak <xjjak@proton.me>
2024-03-18 14:10:05 +0100

    Modified evaluation output and added lables

--- Software/tools/keylogging/comparison-eval.py
+++ Software/tools/keylogging/comparison-eval.py
@@ -2,10 +2,15 @@ import numpy as np
 
 with open("comparing-timing.log", "r") as f:
     vals = []
+    x, y  = map(int, f.readline().strip().split(":"))
+    y = y // 1000
+    print("Timestamp at start: ", y-x)
     for line in f:
         l, c = line.strip().split(":")
-        l, c = int(l), int(c[-11:-3])
+        l, c = int(l), int(c[:-3])
         vals.append(c-l)
 
-print(sum(vals)/len(vals))
-print(np.std(vals))
+a = sum(vals)/len(vals)
+print("Averaged timestamp: ", a)
+print("Difference between start and average: ", (y-x)-a)
+print("Standard deviation: ", np.std(vals))
