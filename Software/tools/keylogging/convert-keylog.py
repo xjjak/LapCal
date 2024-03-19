028e0167e1383d9d19120d74320680337216ae98
Emil Wiards <emil@wiards.net>
2024-03-19 20:47:10 +0100

    Fix keylogger script
    - fixed functions to act on single record instead of a list
    - print timestamp

--- Software/tools/keylogging/convert-keylog.py
+++ Software/tools/keylogging/convert-keylog.py
@@ -2,61 +2,57 @@
 DATA_OF_RIGHT_HAND = False
 
 # List for translation from 
-TRANSLATION = dict(zip(
-    ["00","10","20", "01","11","21", "02","12","22",
-     "03","13","23", "04","14","24", "31","30"],
-    ["00","01","02", "10","11","12", "20","21","22",
-     "30","31","32", "40","41","42", "50","51"]
-))
+LEFT_HAND  = {(0,0), (1,0), (2,0), (3,0), (4,0),
+              (0,1), (1,1), (2,1), (3,1), (4,1),
+              (0,2), (1,2), (2,2), (3,2), (4,2),
+              (0,3), (1,3)}
+RIGHT_HAND = {(0,4), (1,4), (2,4), (3,4), (4,4),
+              (0,5), (1,5), (2,5), (3,5), (4,5),
+              (0,6), (1,6), (2,6), (3,6), (4,6),
+              (0,7), (1,7)}
 
 
 # data entry format: [timestamp, keycode, row, col, layer(?), press_event, ...]
-def preprocess(data):
-    new_data = []
-    for record in data:
-        new_data.append([
-            record[0],
-            record[1],
-            int(record[2]),
-            int(record[3]),
-            int(record[4]),
-            bool(record[5])
-        ])
-    return new_data
+def preprocess(record):
+    return [
+        int(record[0]), # [0] timestamp
+        record[1],      # [1] keycode
+        int(record[2]), # [2] row
+        int(record[3]), # [3] col
+        int(record[4]), # [4] layer
+        bool(int(record[5])) # [5] press_event
+    ]
 
 
-def filter_for_hand(data):
+def filter_for_hand(record):
     global DATA_OF_RIGHT_HAND
-    filtered_data = []
-    for record in data:
-        if DATA_OF_RIGHT_HAND:
-            if record[3] > 4:
-                filtered_data.append(record)
-        else:
-            if record[3] < 5:
-                filtered_data.append(record)
-
-    return filtered_data
-
-# def get_data(file_name=None):
-#     data = []
-#     if file_name:
-#         with open(file_name, "r") as f:
-#             for line in f.read().strip().split('\n'):
-#                 data.append(line.strip().split(','))
-#     else:
-        
-        
-#     return data
-
-def get_key_number(row, col):
-    global TRANSLATION
-
+    global LEFT_HAND
+    global RIGHT_HAND
+    
+    pos = (record[3], record[2])
+    
     if not DATA_OF_RIGHT_HAND:
-        key = str(row) + str(col)
-        number = TRANSLATION[key]
+        if pos in LEFT_HAND:
+            return True
     else:
-        # TODO: right hand inversion
+        if pos in RIGHT_HAND:
+            return True
+
+    return False
+        
+
+# FIXME: unused
+def get_key_number(row, col):
+    global DATA_OF_RIGHT_HAND
+    global LEFT_HAND
+    global RIGHT_HAND
+
+    pos = (row, col)
+    
+    if not DATA_OF_RIGHT_HAND:
+        number = LEFT_HAND.index(pos)
+    else:
+        number = RIGHT_HAND.index(pos)
         
     return number
 
@@ -68,11 +64,12 @@ if __name__ == "__main__":
         except EOFError:
             break
 
-        record = preprocess(line)
-        record = filter_for_hand(record)
+        record = line.split(",")
+        record = preprocess(record)
 
-        number = get_key_number(record[2], record[3])
-        if record[5]:
-            print(f"+ {number}")
-        else:
-            print(f"- {number}")
+        if not filter_for_hand(record):
+            continue
+        
+        time, flag, col, row = record[0], record[5], record[3], record[2]
+
+        print(f"{time//1_000_000},{int(flag)},{col},{row}")
