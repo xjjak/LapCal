0d7ca240ab0a3af2ed24dc2446882a65be0a368c
Emil Wiards <emil@wiards.net>
2024-03-22 22:04:59 +0100

    Check for global hid_listen and make local script location relative
    Calling the `keylogger-for-data-collection.sh` script from another
    directory will result in an error because the local hid_listen script
    will not be in that directory.  Referring to the hid_listen script
    from the keylogger script location (dirname "$0") solves this issue.
    
    Further checking for a global hid_listen script makes things easier
    with a nix-shell environment.

--- Software/tools/keylogging/keylogger-for-data-collection.sh
+++ Software/tools/keylogging/keylogger-for-data-collection.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+hid_listen=$(dirname "$0")/hid_listen
+if [[ ! -e "$hid_listen" ]]; then
+    hid_listen=hid_listen
+fi
+
 get_records(){
-    ./hid_listen | grep -E --line-buffered "(0x[A-F0-9]+,)?[0-9]+,[0-9]+,[0-9]{1,2}" 
+    $hid_listen | grep -E --line-buffered "(0x[A-F0-9]+,)?[0-9]+,[0-9]+,[0-9]{1,2}" 
 }
 
 prepend_timestamp(){
