9104f24394f080025208a3aef2f03081fe405511
Emil Wiards <emil@wiards.net>
2024-03-27 22:12:15 +0100

    Update merge.py
    Merge.py now supports different prepare_dataset types.  These can also
    provide multiple exports (export variable lists all
    prepar_dataset_... functions).

--- Software/processing/merge.py
+++ Software/processing/merge.py
@@ -3,82 +3,83 @@
 
 import sys
 import os
-import numpy as np
-import joblib
-from sklearn.model_selection import train_test_split
-from prepare_dataset import prepare_dataset
+import importlib
+from pathlib import Path
 
-# check if dataset directory was provided
+# Retrieve dataset type and load respective configuration
+dtype = (os.environ['LAPCAL_DATASET_TYPE']
+         if 'LAPCAL_DATASET_TYPE' in os.environ
+         else 'A')
+
+if os.path.exists(Path(__file__).parent / f"prepare_dataset_{dtype}.py"):
+    prepare_dataset = importlib.import_module(f"prepare_dataset_{dtype}")
+else:
+    print(f"ERROR: No configuration for dataset type [{dtype}] found.")
+    exit(1)
+    
+# Check if dataset directory was provided
 if len(sys.argv) > 1:
     dataset_directory = sys.argv[1]
     assert os.path.exists(os.path.join(dataset_directory, "data.txt")) \
-        and os.path.exists(os.path.join(dataset_directory, "clicks.txt"))
+        and os.path.exists(os.path.join(dataset_directory, "clicks.txt")), \
+        f"Could not find data.txt or clicks.txt in {datase_directory}"
 else:
-    print("No directory specified.")
+    print("ERROR: No directory specified.")
     exit(1)
 
-warmup_time = 100
-    
-milliseconds = 0
-readings_with_time = []
-minimum_milliseconds = None
 
-# read readings from data.txt
+import numpy as np
+import joblib
+from sklearn.model_selection import train_test_split
+
+# Read readings from data.txt
+readings_with_time = []
 with open(os.path.join(dataset_directory, "data.txt")) as f:
     data = f.read().split("\n")
-    milliseconds = int(data[0].split(":")[0])
-    last_milliseconds = milliseconds
 
     # Iterate through all entries except
-    # - the first, because it only contains the column names,
+    # - the first, because it only contains the column names or partial information,
     # - the last, because it may be empty or contain partial information.
     for line in data[1:-1]:
-        if "t" in line:
-            readings_with_time += [(last_milliseconds+1, line)]
-        else:
-            reading = line.split(":")
-            last_milliseconds = milliseconds + int(reading[0])//1000
-            minimum_milliseconds = minimum_milliseconds or last_milliseconds
-            readings_with_time += [(last_milliseconds, ":".join(reading[1:]))]
+        reading = line.split(":")
+        readings_with_time += [(int(reading[0]), ":".join(reading[1:]))]
+        
+readings_with_time.sort()
 
-clicks_with_time  = []
-
-# read clicks from clicks.txt
+# Read clicks from clicks.txt
+clicks_with_time = []
 with open(os.path.join(dataset_directory, "clicks.txt")) as f:
-    past_start = False
-    
     for line in f.read().strip().split("\n"):
         click = line.split(",")
-        time = int(click[0])+125
+        clicks_with_time += [(int(click[0]), ",".join(click[1:]))]
         
-        if time > minimum_milliseconds + warmup_time and click[1] == "1":
-            past_start = True
-            
-        if past_start:
-            clicks_with_time += [(time, ",".join(click[1:]))]
+clicks_with_time.sort()
 
-
-# join readings and clicks
+# Write result to joined.txt
 joined = sorted(readings_with_time + clicks_with_time)
-
-# write result to joined.txt
 with open(os.path.join(dataset_directory, "joined.txt"), "w") as f:
     for i in joined:
         f.write(i[1]+"\n")
 
 
-x, y = prepare_dataset(readings_with_time, clicks_with_time)
+# Save a dataset for every preparation function in the given module.
+for prepare_dataset in prepare_dataset.export:
+    x, y = prepare_dataset(readings_with_time, clicks_with_time)
 
-# split into train, dev and test distribution
-x_train, x_dev, y_train, y_dev = train_test_split(
-    x, y, test_size=0.3, random_state=25
-)
-x_dev, x_test, y_dev, y_test = train_test_split(
-    x_dev, y_dev, test_size=0.4, random_state=16
-)
+    # Split into train, dev and test distribution
+    x_train, x_dev, y_train, y_dev = train_test_split(
+        x, y, test_size=0.3, random_state=25
+    )
+    x_dev, x_test, y_dev, y_test = train_test_split(
+        x_dev, y_dev, test_size=0.4, random_state=16
+    )
 
-dataset = (x_train, y_train, x_dev, y_dev, x_test, y_test)
-dataset_full = (x, y)
+    # Get name from function name
+    name = prepare_dataset.__name__
+    if name.startswith("prepare_"):
+        name = name[len("prepare_"):]
+    name = name.replace('_', '-')
 
-joblib.dump(dataset, os.path.join(dataset_directory, "dataset-split.joblib"))
-joblib.dump(dataset_full, os.path.join(dataset_directory, "dataset.joblib"))
+    
+    dataset = (x_train, y_train, x_dev, y_dev, x_test, y_test)
+    joblib.dump(dataset, os.path.join(dataset_directory, f"{name}.joblib"))
