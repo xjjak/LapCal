b649b150f1bcec02fc4e250b596f7e44f709bd3d
Emil Wiards <emil@wiards.net>
2024-03-28 09:32:48 +0100

    working presentation

--- Software/tools/live-preview/model.py
+++ Software/tools/live-preview/model.py
@@ -1,28 +1,48 @@
+from pipe import Pipe
+
 import joblib
+
 import tensorflow as tf
 from tensorflow import keras
 
-class Model:
-    def __init__(self):
+
+class Model(Pipe):
+    def __init__(self, *args, **kwargs):
+        self.ready = False
+        self.result = None
+        self.load(*args, **kwargs)
+        
+    def load(self, *args, **kwargs):
         self.model = None
 
-    def predict(self, features):
+    def predict(self, data):
         return []
+
+    def feed(self, data):
+        self.result = self.predict(data)
+        self.ready = True
+        return True
+
+    def retrieve(self):
+        self.ready = False
+        return self.result
+        
 
 
 # joblib
-class SklearnModel:
-    def __init__(self, path):
+class SklearnModel(Model):
+    def load(self, path):
         self.model = joblib.load(path)
 
     def predict(self, features):
         return self.model.predict([features])
 
 
-class TensorFlowModel:
-    def __init__(self, path):
+class TensorFlowModel(Model):
+    def load(self, path, threshold=0.5):
         self.model = keras.models.load_model(path)
+        self.threshold = threshold
 
     def predict(self, features):
         # print(self.model.predict([features]))
-        return self.model.predict([features])[0] > 0.01
+        return self.model.predict([features])[0] > self.threshold
--- Software/tools/live-preview/preview.py
+++ Software/tools/live-preview/preview.py
@@ -3,26 +3,68 @@ import serial
 class LivePreview:
     def __init__(
             self,
-            feature_extractor,
-            model,
+            *pipes: Pipe
             port = "/dev/ttyUSB0",
             baudrate = 115200,
     ):
         self.serial = serial.Serial(port, baudrate)
-        self.feature_extractor = feature_extractor
-        self.model = model
+        self.pipes = pipes
         self.output = None
 
-    def next_output(self):
-        while not self.feature_extractor.available():
+    # def next_output(self):
+    #     while not self.feature_extractor.available():
+    #         line = self.serial.readline().decode()
+    #         if self.output:
+    #             self.feature_extractor.feed(line, output=self.output)
+    #         else:
+    #             self.feature_extractor.feed(line)
+
+    #     features = self.feature_extractor.get_features()
+    #     self.output = list(self.model.predict(features))
+    #     return self.output
+
+    def next(self):
+        while not self.pipe.available():
             line = self.serial.readline().decode()
             if self.output:
-                self.feature_extractor.feed(line, output=self.output)
+                self.pipe.feed(line, output=self.output)
             else:
-                self.feature_extractor.feed(line)
+                self.pipe.feed(line)
 
-        features = self.feature_extractor.get_features()
-        self.output = list(self.model.predict(features))
+        self.output = self.pipe.retrieve()
+        return self.output
+
+    def preview(self):
+        print("Starting preview...")
+        while True:
+            print(self.next())
+
+
+class MultiPreview:
+    def __init__(
+            self,
+            feature_extractors,
+            models,
+            port = "/dev/ttyUSB0",
+            baudrate = 115200,
+    ):
+        assert len(feature_extractors) == len(models)
+        self.serial = serial.Serial(port, baudrate)
+        self.feature_extractors = feature_extractors
+        self.models = models
+        self.output = [None]*len(models)
+
+    def next_output(self):
+        for index in range(len(self.models)):
+            while not self.feature_extractors[index].available():
+                line = self.serial.readline().decode()
+                if self.output[index]:
+                    self.feature_extractors[index].feed(line, output=self.output[index])
+                else:
+                    self.feature_extractors[index].feed(line)
+
+            features = self.feature_extractors[index].get_features()
+            self.output[index] = list(self.models[index].predict(features))
         return self.output
 
     def preview(self):
