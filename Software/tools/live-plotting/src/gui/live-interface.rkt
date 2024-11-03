ab07fafedeebfabf1b08d6837c77a3e6f15e124b
Emil Wiards <emil@wiards.net>
2024-11-03 11:08:19 +0100

    Clean up `live-interface.rkt`
    Added lots of comments and restructured code to be shallower.
    
    Also added some generalization that we will probably expand on in the
    future.

--- Software/tools/live-plotting/src/gui/live-interface.rkt
+++ Software/tools/live-plotting/src/gui/live-interface.rkt
@@ -1,116 +1,179 @@
 #lang racket/gui
 
-(provide live-interface%)
+;; coordinate plotting canvas and live data source
 
-(require "../processing/parse.rkt")
+(provide
+ ;; coordinate plotting canvas and live data source
+ live-interface%)
 
-(require "ble-serial.rkt")
-(require "volatile-message.rkt")
+;; ==============
+;; implementation
+
+(require "../processing/parse.rkt"
+         "ble-serial.rkt"
+         "volatile-message.rkt")
 
 ;; TODO: extract "view size editor" to separate component
 
 (define live-interface%
   (class vertical-panel%
     (init parent
+          ;; callback on new input from glove
           [(callback on-input)]
+          ;; default indices for plot source
           [default-sensor-index 0]
           [default-value-index 0])
-    (define on-input callback)
-    (define sensor-index default-sensor-index)
-    (define value-index default-value-index)
-
-    (define/public (set-sensor sensor)
-      (set! sensor-index sensor))
-
-    (define/public (set-value value)
-      (set! value-index value))
-
+    
     (super-new [parent parent]
                [horiz-margin 5]
                [min-width 200]
                [alignment '(left top)])
     
-    (define ble-controller
-      (new ble-serial-controller% [parent this]
-           [message-callback
-            (λ (msg)
-              (send feedback-msg show-message msg))]))
-    
+    ;; ----------------
+    ;; public interface
+
+    ;; set current sensor index to given index
+    (define/public (set-sensor sensor)
+      (set! sensor-index sensor))
+
+    ;; set current value index to given index
+    (define/public (set-value value)
+      (set! value-index value))
+
+    ;; disconnect the bluetooth controller
     (define/public (disconnect)
       (send ble-controller disconnect))
 
-    (define ble-serial-port "/tmp/ttyBLE")
-    
-    ;; maintain plotting in different thread
+
+    ;; ---------------
+    ;; class variables
+
+    ;; bind callback
+    (define on-input callback)
+
+    ;; currently active sensor and value index
+    (define sensor-index default-sensor-index)
+    (define value-index default-value-index)
+
+    ;; thread for plotting incoming data
     (define plotting-thread #f)
+
+
+    ;; ----------------
+    ;; helper functions
     
+    (define (msg str #:ttl [ttl #f] #:color [color #f])
+      (send feedback-msg show-message str ttl color))
+
+    
+    ;; ------------
+    ;; gui elements
+    
+    ;; controls bluetooth connection
+    (define ble-controller
+      (new ble-serial-controller% [parent this]
+           [message-callback msg]))
+
+    ;; button to toggle plotting
     (define plot-button
       (new button% [parent this]
            [label "Start plotting"]
            [stretchable-width #t]
            [vert-margin 25]
            [callback
-            (λ (b c)
+            (λ _
               (if plotting-thread
-                  (begin
-                    (when (thread-running? plotting-thread)
-                      (thread-send plotting-thread 'stop))
-                    (send b set-label "Start plotting")
-                    (send ble-controller enable #t)
-                    (set! plotting-thread #f))
-                  (if (file-exists? ble-serial-port)
-                      (begin
-                        (send ble-controller enable #f)
-                        (send b set-label "Stop plotting")
-                        (set! plotting-thread
-                              (thread
-                               (thunk
-                                (define in (open-input-file "/tmp/ttyBLE"))
-                                (define testing-interval 50)
-                                
-                                (let loop ([i testing-interval])
-                                  ;; TODO: move parse handling elsewhere
-                                  (let* ([str (read-line in)]
-                                         [data (parse str)]
-                                         [row (if (vector? data) (vector-ref data sensor-index) #f)]
-                                         [value (if (vector? row) (vector-ref row value-index) #f)])
-                                    (if value
-                                        (on-input value)
-                                        ;; (send plotter push value)
-                                        (send feedback-msg show-message
-                                              "Parsing error: Check sensor connection")))
-                                  (unless (and (= i 0) (eq? (thread-try-receive) 'stop))
-                                    (loop (modulo (sub1 i) testing-interval))))
-                                (close-input-port in)))))
-                      (send feedback-msg show-message (format "Serial port (~a) not found." ble-serial-port)))))]))
-    
-    
-    ;; (define (update-plotter-values)
-    ;;   (let ([size (get-attribute config sensor-index value-index 'size)]
-    ;;         [min (get-attribute config sensor-index value-index 'min)]
-    ;;         [max (get-attribute config sensor-index value-index 'max)])
-    ;;     (send plotter set-size size)
-    ;;     (send plotter set-min-value min)
-    ;;     (send plotter set-max-value max)
-    ;;     (send size-text-field set-value (number->string size))
-    ;;     (send min-text-field set-value (number->string min))
-    ;;     (send max-text-field set-value (number->string max))))
-    
-    
+                  (stop-plotting)
+                  (start-plotting)))]))
+
+    ;; text box for user feedback
     (define feedback-msg
       (new volatile-message% [parent this]
            [color "red"]))
+
+
+    ;; -------------------------
+    ;; plotting thread utilities
+
+    ;; start plotting thread and adjust gui
+    (define (start-plotting)
+      (send ble-controller enable #f)
+      (send plot-button set-label "Stop plotting")
+      (set! plotting-thread (thread thread-fn)))
+
+    ;; terminate plotting thread and adjust gui
+    (define (stop-plotting)
+      (when (and (thread? plotting-thread)
+                 (thread-running? plotting-thread))
+        (thread-send plotting-thread 'stop))
+      (send plot-button set-label "Start plotting")
+      (send ble-controller enable #t)
+      (set! plotting-thread #f))
+
+    ;; repeatedly get relevant value from data source and pass it to
+    ;; callback until thread receives `'stop` signal
+    (define (plotting-loop get-input)
+      (define testing-interval 50)
+      (let loop ([i testing-interval])
+        (let* ([str (get-input)]
+               [data (parse str)]
+               [row (if (vector? data) (vector-ref data sensor-index) #f)]
+               [value (if (vector? row) (vector-ref row value-index) #f)])
+          (if value
+              (on-input value)
+              (msg "Parsing error: Check sensor connection")))
+        (unless (and (= i 0) (eq? (thread-try-receive) 'stop))
+          (loop (modulo (sub1 i) testing-interval)))))
+
     
-    
+    ;; --------------
+    ;; initialization
+
+    ;; setup thread function
+    (define thread-fn
+      (ble-serial-input #:processing-loop plotting-loop
+                        #:ble-serial-port "/tmp/ttyBLE"
+                        #:error (λ (str #:lock lock)
+                                  (when lock
+                                    (send ble-controller enable #f))
+                                  (msg str #:ttl #f))))))
+
+;; --------------------------
+;; thread function generators
+
+;; a collection of functions that generate thread functions required
+;; for handling different data sources
+
+;; generate thread function that uses the ble-serial data source
+(define (ble-serial-input #:processing-loop processing-loop
+                          #:ble-serial-port [ble-serial-port "/tmp/ttyBLE"]
+                          #:error [error void])
+  ;; check if ble-serial is available
+  (cond
+    [(file-exists? ble-serial-port)
+     (error "Port busy.\n Connection probably already established."
+            #:lock #t)]
+    [(not (system "command -v ble-serial"))
+     (error "Can't find ble-serial.\nPlease setup the port yourself."
+            #:lock #t)])
+
+  ;; return thread function
+  (λ ()
     (cond
       [(file-exists? ble-serial-port)
-       (send ble-controller enable #f)
-       (send feedback-msg set-label
-             "Port busy.\n Connection probably already established.")]
-      [(not (and (system "command -v ble-scan")
-                 (system "command -v ble-serial")))
-       (send ble-controller enable #f)
-       (send feedback-msg set-label
-             "Can't find ble-scan and ble-serial.\nPlease setup the port yourself.")]
+       (let* ([port (open-input-file ble-serial-port)])
+         (processing-loop (port-reader port))
+         (close-input-port port))]
       [else
-       (void)])))
+       (error (format "Serial port (~a) not found." ble-serial-port)
+              #:lock #f)])))
+
+;; ----------------
+;; helper functions
+
+;; generate function that reads a line from given port when called
+(define (port-reader port)
+  (let ([in port])
+    (thunk (read-line in))))
+
+
